1046d4a744044a4db9e518b777b5f3952102a254
Zachary Grumbles <zakgrumbles@gmail.com>
2018-05-28 20:46:47 -0500

    db: start SQLAlchemy work
    * Removed alot of db interface code
    * Added modesl for tables
    * added logic for using sqlalchemy in db interfaces

--- src/db/interfaces.py
+++ src/db/interfaces.py
@@ -4,7 +4,10 @@ A set of interfaces used for interacting with rl-brain databases.
 """
 
 from ..utils import logger_factory
-import sqlite3
+from . import models
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, session
 
 
 class RLBrainDBInterface:
@@ -15,13 +18,16 @@ class RLBrainDBInterface:
     """
 
     def save_player(self, player):
-        raise NotImplementedError('\'save_player\' method not implemented')
+        self._db_session.add(player)
+        self._db_session.commit()
 
     def save_match(self, match):
-        raise NotImplementedError('\'save_match\' method not implemented')
+        self._db_session.add(match)
+        self._db_session.commit()
 
-    def save_team(self, match):
-        raise NotImplementedError('\'save_match\' method not implemented')
+    def save_team(self, team):
+        self._db_session.add(team)
+        self._db_session.commit()
 
     def _init_logger(self):
         """
@@ -31,66 +37,16 @@ class RLBrainDBInterface:
         """
         self._logger = logger_factory.make_logger(__name__)
 
+    def __init__(self):
+        self._db_session_maker = sessionmaker(bind=self._db_engine)
+        self._db_session = session.DBSession()
+
 
 class RLBrainSqliteDB(RLBrainDBInterface):
     """SQLite3 interface for rl-brain databases
 
     Interface for interacting/creating an sqlite3 rl-brain database
     """
-
-    def save_player(self, player):
-        pass
-
-    def save_match(self, match):
-        pass
-
-    def save_team(self, team):
-        pass
-
-    def _create_tables(self):
-        query = '''SELECT name
-            FROM sqlite_master
-            WHERE type='table'
-            AND name=\'{}\';'''
-
-        self._cursor.execute(query.format('players'))
-        names = self._cursor.fetchall()
-        if len(names) == 0:
-            from .queries import CREATE_PLAYERS_TABLE
-            self._logger.info('players table not found. Creating...')
-            self._cursor.execute(CREATE_PLAYERS_TABLE)
-        else:
-            self._logger.info('players table found.')
-
-        self._cursor.execute(query.format('matches'))
-        names = self._cursor.fetchall()
-        if len(names) == 0:
-            from .queries import CREATE_MATCHES_TABLE
-            self._logger.info('matches table not found. Creating...')
-            self._cursor.execute(CREATE_MATCHES_TABLE)
-        else:
-            self._logger.info('matches table found.')
-
-        self._cursor.execute(query.format('teams'))
-        names = self._cursor.fetchall()
-        if len(names) == 0:
-            from .queries import CREATE_TEAMS_TABLE_SQLITE
-            self._logger.info('teams table not found. Creating...')
-            self._cursor.execute(CREATE_TEAMS_TABLE_SQLITE)
-        else:
-            self._logger.info('teams table found.')
-
-    def _init_db(self):
-        """Initialize the database
-
-        Creates or connects to the sqlite db located at the given path.
-
-        :return None
-        """
-
-        self._conn = sqlite3.connect(self._db_path)
-        self._cursor = self._conn.cursor()
-        self._create_tables()
 
     def __init__(self, db_path):
         """Constructor
@@ -101,6 +57,7 @@ class RLBrainSqliteDB(RLBrainDBInterface):
         :param db_path: Path to the existing/desired sqlite database
         :return None
         """
-        self._db_path = db_path
         self._init_logger()
-        self._init_db()
+        self._db_path = db_path
+        self._engine = create_engine(f'sqlite:///{self._db_path}')
+        models.Base.metadata.create_all(self._engine)
